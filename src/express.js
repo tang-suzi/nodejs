5970e870b2ba2fe2917f95a800a4e8f2808cd80f
tang-suzi <tangtang960225@163.com>
2018-10-05 19:53:41 +0800

    静态文件

--- src/express.js
+++ src/express.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express();
 
+//静态文件
+app.use(express.static('./public'));
+
 app.get('/', function(req, res){
     res.end('hello\n')
-})
+});
 
 app.listen(18000, function afterListen(){
     console.log('express runn on http://localhost:18000')
