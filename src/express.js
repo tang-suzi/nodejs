ea1b73dd618902be30e53d8a6d94293d00713724
tang-suzi <tangtang960225@163.com>
2018-10-05 21:02:31 +0800

    express路由

--- src/express.js
+++ src/express.js
@@ -5,10 +5,44 @@ var app = express();
 //静态文件
 app.use(express.static('./public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.end('hello\n')
 });
 
-app.listen(18000, function afterListen(){
+// Router
+var Router = express.Router();
+/**
+ * http://example.com/post/add
+ * http://rxample.com/post/list
+ */
+Router.get('/add', function (req, res) {
+    res.end('Router /app\n');
+})
+Router.get('/list', function (req, res) {
+    res.end('Router /list\n');
+})
+
+app.use('/post', Router);
+
+//route
+app.route('/article')
+.get(function(req, res){
+    res.end('route /article get\n');
+})
+.post(function(req, res){
+    res.end('route /article post\n');
+});
+
+//http://example.com/news/123
+app.param('newsId', function(req,res,next,newsId){
+    req.newsId = newsId;
+    next();
+});
+
+app.get('/news/:newsId', function(req,res){
+    res.end('newsId' + req.newsId + '\n')
+})
+
+app.listen(18000, function afterListen() {
     console.log('express runn on http://localhost:18000')
 })
